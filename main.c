0ed718b7c8ea2bc297666256caf229a9ef056eb1
Angelo Ortiz Vega <angelo2898@gmail.com>
2019-09-17 15:53:11 -0600

    [INIT] Setup Project

---
+++ main.c
@@ -0,0 +1,19 @@
+/* 
+    main.c -- archivo principal para correr aplicación cliente de DonCEKongJr.
+
+    Este archivo es parte de DonCE Y Kong Jr, el mismo corresponde al Proyecto III para el curso de Lenguajes, Compiladores e Intérpretes. (CE3104), Módulo Lenguajes.
+    El proyecto consiste en la implementación de una aplicación que permita reafirmar el conocimiento de los paradigmas de programación imperativo y orientado a objetos.
+    Para más información acerca del proyecto visite: https://github.com/ce-itcr/DonCEyKongJr
+
+    Versión de Archivo  :   1.0
+    Autores             :   Github@angelortizv
+    Última Modificación :   17/09/2019, 16:00, @angelortizv
+
+*/
+
+#include <stdio.h>
+#include <SDL2/SDL.h>
+
+int main(int argc, char *argv[]){
+
+}
---
+++ src/controller.c
@@ -0,0 +1,16 @@
+/* 
+    controller.c -- archivo controlador entre ventanas y objetos de aplicación
+
+    Este archivo es parte de DonCE Y Kong Jr, el mismo corresponde al Proyecto III para el curso de Lenguajes, Compiladores e Intérpretes. (CE3104), Módulo Lenguajes.
+    El proyecto consiste en la implementación de una aplicación que permita reafirmar el conocimiento de los paradigmas de programación imperativo y orientado a objetos.
+    Para más información acerca del proyecto visite: https://github.com/ce-itcr/DonCEyKongJr
+
+    Versión de Archivo  :   1.0
+    Autores             :   Github@angelortizv
+    Última Modificación :   17/09/2019, 16:00, @angelortizv
+
+*/
+
+#include <stdio.h>
+#include <SDL2/SDL.h>
+
