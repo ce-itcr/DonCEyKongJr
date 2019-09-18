cc88e32644a0016c607d54a849e17d4a7802bbec
Angelo Ortiz Vega <angelo2898@gmail.com>
2019-09-18 15:51:45 -0600

    [UPDATE] Code Share

--- main.c
+++ main.c
@@ -1,14 +1,13 @@
 /* 
-    main.c -- archivo principal para correr aplicación cliente de DonCEKongJr.
+ main.c - main file to run the DonCEKongJr client application.
 
-    Este archivo es parte de DonCE Y Kong Jr, el mismo corresponde al Proyecto III para el curso de Lenguajes, Compiladores e Intérpretes. (CE3104), Módulo Lenguajes.
-    El proyecto consiste en la implementación de una aplicación que permita reafirmar el conocimiento de los paradigmas de programación imperativo y orientado a objetos.
-    Para más información acerca del proyecto visite: https://github.com/ce-itcr/DonCEyKongJr
+ This file is part of DonCE Y Kong Jr, the same correspondent to Project III for the course of Languages, Compilers and Interpreters. (CE3104), Languages module.
+ The project consists in the implementation of an application that reaffirms the knowledge of the imperative and object-oriented programming paradigms.
+ For more information about the project visit: https://github.com/ce-itcr/DonCEyKongJr
 
-    Versión de Archivo  :   1.0
-    Autores             :   Github@angelortizv
-    Última Modificación :   17/09/2019, 16:00, @angelortizv
-
+ File Version    : 1.0
+ Authors         : Github @ angelortizv
+ Last modified   : 09/17/2019, 16:00, @angelortizv
 */
 
 #include <stdio.h>
